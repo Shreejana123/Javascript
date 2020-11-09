4c0661e94d4ddd6f07949fb6bf565b03e62a8563
SUMAN <shreejanatandukar2@gmail.com>
2020-11-09 22:11:02 +0545

    first exercise on variables.

---
+++ fortuneteller.js
@@ -0,0 +1,6 @@
+var numKids = 2;
+var partnerName = 'Ram';
+var geoLocation = 'Kathmandu';
+var jobTitle = 'web developer';
+var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', ' + 'and married to ' + partnerName + ' with '+ numKids + 'kids.';
+console.log(future);
