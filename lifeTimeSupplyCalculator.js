49e82779573c129603e2291f5b0f2a0b6191ffa2
SUMAN <shreejanatandukar2@gmail.com>
2020-11-09 22:30:20 +0545

    third exercise of variables

---
+++ lifeTimeSupplyCalculator.js
@@ -0,0 +1,6 @@
+var currentAge = 10;
+var maxAge = 70;
+var estimatedDay = 3;
+var totalNeeded = (estimatedDay * 365) * (maxAge - currentAge);
+var result = 'You will need ' + totalNeeded + ' times of snack to last you until the ripe old age of ' + maxAge;
+console.log(result);
