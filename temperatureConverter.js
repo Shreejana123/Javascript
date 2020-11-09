4b2d0e7f28cc183f281adc3a38305e14efadd4e6
SUMAN <shreejanatandukar2@gmail.com>
2020-11-09 22:49:34 +0545

    fifth exercise on variables.

---
+++ temperatureConverter.js
@@ -0,0 +1,8 @@
+var celcius = 100;
+var farenheit = celcius * (9/5) + 32;
+var output = `${celcius} is ${farenheit}`;
+var farTemp = 32;
+var celTemp = (farTemp - 32) * (5/9);
+var result = `${farTemp} is ${celTemp}`;
+console.log(output);
+console.log(result);
