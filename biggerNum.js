be764c62001d2ac0d21c0ba0637c124414d51042
SUMAN <shreejanatandukar2@gmail.com>
2020-11-10 21:31:12 +0545

    to find greatest number

---
+++ biggerNum.js
@@ -0,0 +1,9 @@
+function greaterNum(num1, num2){
+    if(num1 > num2){
+        console.log(num1 + ' is greater.');
+    }else{
+        console.log(num2 + ' is greater.');
+    }
+}
+greaterNum(1, 2);
+greaterNum(7, 10);
