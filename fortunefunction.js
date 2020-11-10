d38e5d478a4576921700440a96bd5733df9ee603
SUMAN <shreejanatandukar2@gmail.com>
2020-11-10 20:44:53 +0545

    first exercise on function

---
+++ fortunefunction.js
@@ -0,0 +1,7 @@
+function tellFortune(jobTitle, geoLocation, partnerName, numKids){
+    var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', ' + 'and married to ' + partnerName + ' with ' + numKids + ' kids.';
+    console.log(future);
+}
+tellFortune('web developer', 'kathmandu', 'David', '2');
+tellFortune('content editor', 'pokhara', 'shyam', '1');
+tellFortune('graphic designer', 'biratnagar', 'ram', '1');
