973c81b1c07ea400bd0009c67fa1b9f367c1f70c
SUMAN <shreejanatandukar2@gmail.com>
2020-11-10 20:55:09 +0545

    second exeercise on function

---
+++ puppyAgeCalculator.js
@@ -0,0 +1,7 @@
+function calculateDogAge(puppyAge){
+    var dogYears = 7*puppyAge;
+    console.log("Your doggie is " + dogYears + " years old in dog years!"); 
+}
+calculateDogAge(1);
+calculateDogAge(2);
+calculateDogAge(3);
