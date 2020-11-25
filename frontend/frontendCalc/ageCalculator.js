7d3a9b68016ab62df97f43e5aa20453e9acaeca1
SUMAN <shreejanatandukar2@gmail.com>
2020-11-25 22:00:02 +0545

    Add Calculator examples

--- frontend/frontendCalc/ageCalculator.js
+++ frontend/frontendCalc/ageCalculator.js
@@ -1,5 +1,6 @@
-function calculate_age(dob){
-    let currentDate, birthDate;
+let currentDate, birthDate;
+function ageCalculator(currentDate, birthDate){
+
     let diff = currentDate - birthDate;
     let age = new Date(diff); 
     return Math.abs(age_dt.getUTCFullYear() - 1970);
